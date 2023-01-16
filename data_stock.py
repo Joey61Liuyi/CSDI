0a1611fdd13c2e4c8ca9ca87d4f6bf06a7ae925c
JoeyLau <csyiliu@comp.polyu.edu.hk>
2023-01-16 19:23:36 +0800

    merged all data together and fill the nan with 0

--- data_stock.py
+++ data_stock.py
@@ -1,35 +1,39 @@
 import pandas as pd
 import yfinance as yf
-import copy
 
+# data = pd.read_csv('dow_jones.csv')
+# total_lenth = len(data)
+# index = 0
+# data_new = pd.DataFrame([])
 
-data = pd.read_csv('dow_jones.csv')
-total_lenth = len(data)
-index = 0
-data_new = pd.DataFrame([])
+# for i in data.Ticker:
+#     stock = yf.Ticker(i)
+#     # tep = stock.info
+#     his = stock.history(period = "10y")
+#     # tep = yf.Ticker(i).history(period='max')
+#     # print(len(tep))
+#     index += 1
+#     if len(his) < 2519:
+#         print('{}/{}, {} failed'.format(index, total_lenth, i))
+#     else:
+#         tep = (his['High']-his['Low'])/2+his['Low']
+#         data_new[i] = tep
+#         print('{}/{}, {} done'.format(index, total_lenth, i))
+#
+# data_new.to_csv('USA_stock_10y.csv')
 
-for i in data.Ticker:
-    stock = yf.Ticker(i)
-    # tep = stock.info
-    his = stock.history(period = "10y")
-    # tep = yf.Ticker(i).history(period='max')
-    # print(len(tep))
-    index += 1
-    if len(his) < 2519:
-        print('{}/{}, {} failed'.format(index, total_lenth, i))
-    else:
-        tep = (his['High']-his['Low'])/2+his['Low']
-        data_new[i] = tep
-        print('{}/{}, {} done'.format(index, total_lenth, i))
+usa_data = pd.read_csv('USA_stock_10y.csv')
+usa_data['Date'] = [i[:10] for i in usa_data['Date']]
 
-data_new.to_csv('USA_stock_10y.csv')
+hk_data = pd.read_csv('hongkong_stock_10y.csv')
+hk_data['Date'] = [i[:10] for i in hk_data['Date']]
+eu_data = pd.read_csv('EU_stock_10y.csv')
+eu_data['Date'] = [i[:10] for i in eu_data['Date']]
 
-# msft = yf.Ticker('AXP')
-# Dow30 =  []
+tep = pd.merge(usa_data, hk_data, on='Date', how='outer')
+tep = pd.merge(tep, eu_data, on='Date', how='outer')
+tep.set_index(['Date'], inplace=True)
+tep.sort_index(inplace=True)
+tep = tep.fillna(0)
+tep.to_csv('stockdata_all.csv')
 
-
-
-# info = msft.info
-
-# hist = msft.history(period = 'max')
-# print('pause')
