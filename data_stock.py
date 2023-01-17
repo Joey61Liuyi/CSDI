0565a678b30d76303ac8ac0ec5bca191c16ab042
JoeyLau <csyiliu@comp.polyu.edu.hk>
2023-01-17 16:23:42 +0800

    I standardized the dataset

--- data_stock.py
+++ data_stock.py
@@ -34,6 +34,33 @@ tep = pd.merge(usa_data, hk_data, on='Date', how='outer')
 tep = pd.merge(tep, eu_data, on='Date', how='outer')
 tep.set_index(['Date'], inplace=True)
 tep.sort_index(inplace=True)
-tep = tep.fillna(0)
+# tep = tep.fillna(0)
 tep.to_csv('stockdata_all.csv')
 
+data = pd.read_csv('stockdata_all.csv')
+data.set_index(['Date'], inplace=True)
+for c in data.columns:
+    data[c] = (data[c]-data[c].mean())/data[c].std()
+
+data = data.fillna(0)
+
+data = data.to_numpy()
+import numpy as np
+time_lenth = len(data)//11
+remain_num = len(data)%11
+data = data[-(len(data)-remain_num):]
+data = np.split(data, 11, 0)
+np.random.shuffle(data)
+train_data = data[:10]
+test_data = data[-1:]
+#
+train_data = np.array(train_data)
+test_data = np.array(test_data)
+np.save('stock_train.npy', train_data, allow_pickle=True)
+np.save('stock_test.npy', test_data, allow_pickle=True)
+
+# import numpy as np
+# train_data =np.load('stock_test.npy')
+# np.random.shuffle(np.transpose(data))
+# data = np.array(data)
+print('ok')
